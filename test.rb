d12d42a540df412b0407ac86f9ed83ba0eb4b9e5
robertburrowes <r.burrowes@auckland.ac.nz>
2020-09-02 16:17:34 +1200

    Remove commented out code and tidy up

--- test.rb
+++ test.rb
@@ -3,17 +3,10 @@
 require 'tmpfile'
 include TmpFileMod
 
-
-=begin
-t = TmpFile.new("/tmp/pp","w")
-puts t.to_s
-t.puts "Hello World"
-=end
-
 TmpFile.open('/tmp/xxx','w+') do |fd|
   #fd.no_unlink #Uncomment and the test file will not be deleted.
   fd.puts "Hello World #{Time.now}", 10
-  fd.print "Another"
+  fd.print "Another "
   fd.print "Line\n"
   fd.rewind
   fd.each_line do |l|
